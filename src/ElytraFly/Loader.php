e294c4da6096922b6dc1eda62de4ab429e9316f8
KRUNCHSHooT1140 <65598325+KRUNCHSHooT1140@users.noreply.github.com>
2020-07-22 02:55:27 +0700

    Add files via upload

---
+++ src/ElytraFly/Loader.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ElytraFly;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\entity\EntityArmorChangeEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+
+class Loader extends PluginBase implements Listener
+{
+	
+	public function onEnable(){
+		$this->getLogger()->info("ElytraFly Enable");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		if(!$this->getDescription()->getAuthors() === "KRUNCH7SHooT"){
+			$this->getPluginLoader()->disablePlugin($this);
+		}
+	}
+	
+	public function onArmor(EntityArmorChangeEvent $ev){
+		$sender = $ev->getEntity();
+		$item = $ev->getNewItem();
+		if($item->getId() == 444){
+			$sender->setAllowFlight(true);
+			$sender->setFlying(true);
+			$volume = mt_rand();
+			$sender->getLevel()->broadcastLevelSoundEvent($sender, LevelSoundEventPacket::SOUND_LEVELUP, (int) $volume);
+		} else {
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+		if(!$item->getId() == 444){
+			$sender->setAllowFlight(false);
+			$sender->setFlying(false);
+		}
+	}
+}
